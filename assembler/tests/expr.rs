c0ffd8cb13c423b14861d9ebfe7f143dbfc0ddc5
Nazar Yestayev <legion2440@mail.ru>
2026-09-15 15:58:36 +0500

    test(assembler): cover expression evaluation

---
+++ assembler/tests/expr.rs
@@ -0,0 +1,11 @@
+use std::collections::HashMap;
+use corewar_asm::expr::parse_expression;
+
+#[test]
+fn expression_precedence_and_labels() {
+    let mut labels = HashMap::new();
+    labels.insert("x".to_string(), 20usize);
+    assert_eq!(parse_expression("1 + 2 * 3").unwrap().eval(&labels, 0).unwrap(), 7);
+    assert_eq!(parse_expression("(:x - 2) / 3").unwrap().eval(&labels, 5).unwrap(), 13 / 3);
+    assert_eq!(parse_expression("-(4 + 2)").unwrap().eval(&labels, 0).unwrap(), -6);
+}
